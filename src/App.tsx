41d5db560f1ba3c72921efeb0f5d3c93cd52c874
Artsiom-Chulc <artemorsha@mail.ru>
2023-03-11 00:56:16 +0300

    1 part task end

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { Input } from './components/Input';
 
 function App() {
+
+  let [message, setMessage] = useState(
+    [
+      { message: "message1" },
+      { message: "message2" },
+      { message: "message3" },
+      { message: "message4" },
+    ]
+  )
+
+  const addMessage = (title: string) => {
+    let newMessage = { message: title };
+    setMessage([newMessage, ...message])
+  }
+
+  let messages = message.map((el, i) => {
+    return (
+      <div key={i}>{el.message}</div>
+    )
+  })
+
   return (
     <div className="App">
-
+      <Input callBack={addMessage} />
+      {messages}
     </div>
   );
 }
